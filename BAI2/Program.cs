d707e94c0c032260497ada985e7b549d48bb8508
khatu39 <phankhatu123@gmail.com>
2017-10-03 11:51:07 +0700

    baitapb1

---
+++ BAI2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+
+namespace Bai2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPHostEntry ipHostEntry = new IPHostEntry();
+            Console.Write("Enter hostname/IP: ");
+            string input = Console.ReadLine();
+            ipHostEntry = Dns.GetHostEntry(input);
+
+            try
+            {
+                Console.WriteLine("Hostname: " + ipHostEntry.HostName);
+                Console.WriteLine("IPs: ");
+                foreach (IPAddress ip in ipHostEntry.AddressList)
+                {
+                    Console.WriteLine(ip.ToString());
+                }
+                Console.WriteLine("Aliases: ");
+                if (ipHostEntry.Aliases.Length == 0)
+                {
+                    Console.WriteLine("No aliases");
+                }
+                else
+                {
+                    foreach (string alias in ipHostEntry.Aliases)
+                    {
+                        Console.WriteLine(alias);
+                    }
+                }
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.Message);
+            }
+        }
+    }
+}
